53fb6f95766ee7a9e0c80dcbe6df8526c854cac6
Abduqodir Musayev <abduqodirmusayeff@gmail.com>
2023-02-10 07:55:04 +0500

    tasks done except homework

--- tasks/task1.go
+++ tasks/task1.go
@@ -1,5 +1,88 @@
-package tasks
+package main
 
-func Sum(a, b int) int {
-	return a + b
+import (
+	"fmt"
+)
+
+type Record struct {
+	id    string
+	name  string
+	email string
+}
+
+type Database interface {
+	Insert(record Record)
+	Update(record Record)
+	Delete(record Record)
+	GetAll(limit, offset int) []Record
+}
+
+func (i *InMemoryDB) Insert(req Record) {
+	for _, v := range i.records {
+		if v.id == req.id {
+			fmt.Println("ERROR")
+		} else if v.id != req.id {
+			fmt.Println("not exist this User3")
+			return
+		}
+	}
+	i.records = append(i.records, req)
+	fmt.Println(req)
+}
+func (i *InMemoryDB) Update(req Record) {
+	for idx, v := range i.records {
+		if v.id == req.id {
+			i.records[idx].name = req.name
+			i.records[idx].email = req.email
+		} else if v.id != req.id {
+			fmt.Println("not exist this User")
+			return
+		}
+	}
+	fmt.Println(req)
+}
+func (i *InMemoryDB) Delete(req Record) {
+	for ind, v := range i.records {
+		if v.id == req.id {
+			i.records = append(i.records[ind:], i.records[:ind]...)
+			fmt.Println(req)
+		} else if v.id != req.id {
+			fmt.Println("not exist this User2")
+			return
+		}
+	}
+}
+
+func (i *InMemoryDB) GetAll() []Record {
+	return i.records
+}
+
+type InMemoryDB struct {
+	records []Record
+}
+
+func main() {	
+	obj1 := Record{
+		id:    "1",
+		name:  "John",
+		email: "John@gmail.com",
+	}
+	obj2 := Record{
+		id:    "2",
+		name:  "mike",
+		email: "mike@gmail.com",
+	}
+	obj3 := Record{
+		id:    "3",
+		name:  "Peter",
+		email: "Peter@gmail.com",
+	}
+	indb := InMemoryDB{}
+	indb.Insert(obj2)
+	indb.Delete(obj3)
+	indb.Update(obj1)
+	indb.Update(obj2)
+	indb.Update(obj3)
+	res := indb.GetAll()
+	fmt.Println(res)
 }
--- tasks/task2.go
+++ tasks/task2.go
@@ -1,5 +1,76 @@
-package tasks
+package main
 
-func Multiply(a, b int) int {
-	return a * b
+import "fmt"
+
+type User struct {
+	Username string
+	Password string
+}
+
+type Auth interface {
+	Register(user User) error
+	Login(username, password string) bool
+}
+
+func (i *InMemoryAuth) Register(req User) {
+	for _, v := range i.users {
+		if v.Username == req.Username {
+			fmt.Println("this Username already exist")
+			return
+		} else {
+			i.users = append(i.users, req)
+			fmt.Println("added")
+		}
+	}
+}
+func (i *InMemoryAuth) Login(req User) bool {
+	for _, v := range i.users {
+		if v.Username == req.Username && v.Password == req.Password {
+			return true
+		}
+
+	}
+	return false
+}
+
+type InMemoryAuth struct {
+	users []User
+}
+
+func main() {
+
+	Db := InMemoryAuth{}
+	for {
+		fmt.Println("1. Register")
+		fmt.Println("2. Login")
+		fmt.Println("0. tamonlash")
+		var input int
+		fmt.Scan(&input)
+
+		if input == 1 {
+			var register_name string
+			fmt.Println("username kiriting")
+			fmt.Scan(&register_name)
+			var register_pas string
+			fmt.Println("password kiriting")
+			fmt.Scan(&register_pas)
+			Db.Register(User{Username: register_name, Password: register_pas})
+			fmt.Println("registratsiyadan o'tdingiz")
+			return
+
+		} else if input == 2 {
+			var login_name string
+			fmt.Println("Username kiriting")
+			fmt.Scan(&login_name)
+			var login_pas string
+			fmt.Println("password kiriting")
+			fmt.Scan(&login_pas)
+			err := Db.Login(User{Username: login_name, Password: login_pas})
+			if !err {
+				fmt.Println("error")
+			}
+		} else if input == 0 {
+			return
+		}
+	}
 }
